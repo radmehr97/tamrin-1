66ae94d3362782b31328b38dd0815524231fd5c6
Radmehr <122023861+radmehr97@users.noreply.github.com>
2024-11-16 21:20:40 +0330

    Add files via upload

---
+++ tamrin-1/tamrin 2.py
@@ -0,0 +1,7 @@
+
+
+inches=float(input('meghdar inch'))
+
+
+print(inches * 2.54)
+
---
+++ tamrin-1/tamrin 5.py
@@ -0,0 +1,7 @@
+matn = str(input('matn vorodi : '))
+
+tedad = int(input('tedada tekrar :'))
+
+natije=matn*tedad
+
+print(natije)
