df7b0d3483f98cc41a0e081e68076a2187a81022
Radmehr <122023861+radmehr97@users.noreply.github.com>
2024-11-25 10:44:48 +0330

    Add files via upload

---
+++ tamrin-1/tamrin 3.py
@@ -0,0 +1,9 @@
+r=float(input('shoaa : '))
+
+h=float(input('ertefa : '))
+
+hajm = 3.14 * r * r * h
+masahat = 2 * 3.14 * r * h + 2 * 3.14 * r * r
+
+print ('hajm : ',hajm)
+print ('masahat : ',masahat)
